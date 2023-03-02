abd6c1295a80eb235134119d14a6e62b1c84dae8
Sethuraman <iramshiv@gmail.com>
2023-03-02 22:54:54 +0100

    Add files via upload

---
+++ view_dup.py
@@ -0,0 +1,39 @@
+import functools
+import re
+import webbrowser
+
+import pandas as pd
+
+
+def dup_view(df1, xxx, db_filename, li):
+    template = """"
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <head>
+    <p> <b> Duplicate VALUES </b> </p>
+    <p> coun </p>
+    </head>
+    <style>
+    th {
+        background: #2d2d71;
+        color: white;
+        text-align: left;
+    }
+    </style>
+    <body>
+    %s
+    </body>
+    """
+
+    x = df1.style.set_properties(**{'border': '1.3px solid green', 'color': 'black'}).highlight_null(color='yellow') \
+        .hide_index().apply(lambda x: ["background: red" if v in li else "" for v in x], axis = 1)
+    classes = 'table table-striped table-bordered table-hover table-sm'
+    html = template % x.to_html(classes=classes)
+    html = re.sub("<p> coun </p>", f'<p><b> Total DMV Values: {xxx} </b></p>', html)
+    text_file = open(f"./view_error/{db_filename}_view_dup.html", "w", encoding="utf-8")
+    text_file.write(html)
+    text_file.close()
+    pa = f"file:///C:/Users/Sethu/PycharmProjects/errorDetection_Metadata/view_error/{db_filename}_view_dup.html"
+    webbrowser.open(pa, new=2)
+
+
+
